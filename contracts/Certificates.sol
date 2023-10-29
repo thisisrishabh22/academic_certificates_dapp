c2f8d42c3a68826ca4b7685f1c20772065c8a496
Rishabh Singh <ris8645208@gmail.com>
2023-10-29 19:53:02 +0530

    feat: add fileName in the contract

--- contracts/Certificates.sol
+++ contracts/Certificates.sol
@@ -2,35 +2,55 @@
 pragma solidity ^0.8.4;
 
 contract Certificates {
-    mapping(address => mapping(string => bool)) public certificates;
+    struct CertificateInfo {
+        string imageCID;
+        string fileName; // Added field to store the filename
+    }
+
+    mapping(address => mapping(string => CertificateInfo)) public certificates;
     mapping(address => string[]) public studentCertificates;
 
     function verifyCertificate(
         address student,
         string memory certificateHash
     ) external view returns (bool) {
-        return certificates[student][certificateHash];
+        return bytes(certificates[student][certificateHash].imageCID).length > 0;
     }
 
     event CertificateAdded(
         address student,
         string certificateHash,
-        string imageCID
+        string imageCID,
+        string fileName
     );
 
     function addCertificate(
         address student,
         string memory certificateHash,
-        string memory image
+        string memory imageCID,
+        string memory fileName
     ) external {
-        certificates[student][certificateHash] = true;
-        studentCertificates[student].push(image);
-        emit CertificateAdded(student, certificateHash, image);
+        certificates[student][certificateHash] = CertificateInfo({
+            imageCID: imageCID,
+            fileName: fileName
+        });
+
+        studentCertificates[student].push(certificateHash);
+
+        emit CertificateAdded(student, certificateHash, imageCID, fileName);
     }
 
     function fetchStudentCertificates(
         address student
-    ) external view returns (string[] memory) {
-        return studentCertificates[student];
+    ) external view returns (CertificateInfo[] memory) {
+        uint256 length = studentCertificates[student].length;
+        CertificateInfo[] memory result = new CertificateInfo[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            string memory certificateHash = studentCertificates[student][i];
+            result[i] = certificates[student][certificateHash];
+        }
+
+        return result;
     }
 }
--- test/Certificates.test.js
+++ test/Certificates.test.js
@@ -11,34 +11,53 @@ contract('Certificates', (accounts) => {
     const student = accounts[0];
     const certificateCID = 'certificate123';
     const certificateHash = 'certificateHash123';
+    const fileName = 'certificate123.jpg';
 
-    await certificatesInstance.addCertificate(student, certificateHash, certificateCID);
+    await certificatesInstance.addCertificate(student, certificateHash, certificateCID, fileName);
+    const certificateInfo = await certificatesInstance.certificates(student, certificateHash);
+
+    assert.equal(certificateInfo.imageCID, certificateCID, 'Image CID should match');
+    assert.equal(certificateInfo.fileName, fileName, 'File name should match');
   });
 
   it('should verify certificates', async () => {
     const student = accounts[0];
     const certificateCID = 'certificate123';
     const certificateHash = 'certificateHash123';
+    const fileName = 'certificate123.jpg';
 
-    await certificatesInstance.addCertificate(student, certificateHash, certificateCID);
+    await certificatesInstance.addCertificate(student, certificateHash, certificateCID, fileName);
 
     const isCertificateValid = await certificatesInstance.verifyCertificate(student, certificateHash);
     assert.equal(isCertificateValid, true, 'Certificate should be valid');
   });
 
-  it('should fetch certificate CIDs for a student', async () => {
+  it('should fetch certificate info for a student', async () => {
     const student = accounts[0];
     const certificateCID1 = 'certificate456';
     const certificateHash1 = 'certificateHash456';
+    const fileName1 = 'certificate456.jpg';
     const certificateCID2 = 'certificate789';
     const certificateHash2 = 'certificateHash789';
+    const fileName2 = 'certificate789.jpg';
 
-    await certificatesInstance.addCertificate(student, certificateHash1, certificateCID1);
-    await certificatesInstance.addCertificate(student, certificateHash2, certificateCID2);
-    const expectedCertificates = [certificateCID1, certificateCID2];
+    await certificatesInstance.addCertificate(student, certificateHash1, certificateCID1, fileName1);
+    await certificatesInstance.addCertificate(student, certificateHash2, certificateCID2, fileName2);
+
+    const expectedCertificates = [
+      { imageCID: certificateCID1, fileName: fileName1 },
+      { imageCID: certificateCID2, fileName: fileName2 }
+    ];
 
     const fetchedCertificates = await certificatesInstance.fetchStudentCertificates(student);
 
-    assert.deepEqual(fetchedCertificates, expectedCertificates, 'Certificate CIDs should match');
+    const formattedFetchedCertificates = fetchedCertificates.map(certificate => {
+      return {
+        imageCID: certificate[0],
+        fileName: certificate[1]
+      }
+    });
+
+    assert.deepEqual(formattedFetchedCertificates, expectedCertificates, 'Certificate info should match');
   });
 });
